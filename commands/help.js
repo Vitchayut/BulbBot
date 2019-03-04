c548386d4c6fd3f48acccf502e96c7a0cb8721b2
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-04 15:25:29 +0700

    Update and rename ping.js to help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,19 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, prefix) => {
 
-message.channel.send(`:ping_pong: Pong! \`${bot.pings[0]}ms\``);
+let dm = new Discord.RichEmbed()
+    .setColor(`#409cd9`)
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
+    .setDescription(`:mailbox_with_mail:  I have private messaged you a list of commands!`)
+    .setTimestamp()
+    message.channel.send(dm);
+    
+let helpembed = new Discord.RichEmbed()
+    .setColor(`#409cd9`)
+    .setAuthor(`Commands & Guides`, `https://cdn.discordapp.com/avatars/482795587045949440/c364a04cf589085867b25304c60abb26.png`)
+    .addField(`:desktop: General Commands`, `${prefix}help - Show a list of commands & guides to your DM.\n${prefix}ping - Show the current bot ping/ms.`)
+    .setTimestamp()
+    message.author.send(helpembed);  
 
 }
 module.exports.help = {
