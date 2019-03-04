5c0cecd31422e9d420485b947499dd978c677afc
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-04 13:31:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -47,14 +47,14 @@ bot.on("message", async message => {
   if (cmd === `${prefix}help`){
     let DM = new Discord.RichEmbed()
     .setColor(`#409cd9`)
-    .setAuthor(message.member.username, message.author.displayAvatarURL)
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
     .setDescription(`:mailbox_with_mail:  I have private messaged you a list of commands!`)
     .setTimestamp()
     message.channel.send(DM);
     
     let helpembed = new Discord.RichEmbed()
     .setColor(`#409cd9`)
-    .setAuthor(`Commands & Guides`, `message.author.displayAvatarURL`)
+    .setAuthor(`Commands & Guides`, `https://cdn.discordapp.com/avatars/482795587045949440/c364a04cf589085867b25304c60abb26.png`)
     .addField(`:desktop: General Commands`, `${prefix}help - Show a list of commands & guides to your DM.\n${prefix}ping - Show the current bot ping/ms.`)
     .setTimestamp()
     message.author.send(helpembed);
