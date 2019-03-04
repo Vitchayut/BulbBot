508449773c7f46d744bc8cb1f5bd77d93dfa752e
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-04 15:19:19 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args, prefix) => {
 
 message.channel.send(`:ping_pong: Pong! \`${bot.pings[0]}ms\``);
 
