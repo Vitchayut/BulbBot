8e6acba727832cd38204c66d880f4eb910f3797f
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-06 07:59:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ bot.on("message", async message => {
   if (message.channel.type === "dm") return;
 
   let prefix = '!';
+  if(!message.content.startsWith(prefix)) return;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
