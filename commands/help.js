208ded7f70eac263346b99964d07ebadd4e29bf1
NuaZaKung <tanadhpianpark@gmail.com>
2019-03-06 08:48:44 +0700

    Added some new commands. (previously from Frisbee Max)

--- commands/help.js
+++ commands/help.js
@@ -8,13 +8,13 @@ let dm = new Discord.RichEmbed()
     .setDescription(`:mailbox_with_mail: I have private messaged you a list of commands!`)
     .setTimestamp()
     message.channel.send(dm);
-    
+
 let helpembed = new Discord.RichEmbed()
     .setColor(`#409cd9`)
     .setAuthor(`Commands & Guides`, bot.user.avatarURL)
-    .addField(`:desktop: General Commands`, `!help - Show a list of commands & guides to your DM.\n!ping - Show the current bot ping/ms.\n!verify/verification - Verify your discord account.`)
+    .addField(`:desktop: General Commands`, `!help - Show a list of commands & guides to your DM.\n!ping - Show the current bot ping/ms.\n!verify/verification - Verify your discord account.\n!botinfo - Shows the information of the bot.\n!serverinfo - Shows the information of the guild.\n!8ball [Question] - A fun command that answer your question.`)
     .setTimestamp()
-    message.author.send(helpembed);  
+    message.author.send(helpembed);
 
 }
 module.exports.help = {
