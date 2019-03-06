8f35842973fde329f21d35192935a7854b0fa3a8
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-06 12:59:50 +0700

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args, prefix) => {
+module.exports.run = async (bot, message, args) => {
 
     message.delete();
     let role = message.guild.roles.find(role => role.name === 'Verified Member ✔️');
