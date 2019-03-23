5ec9167b26c7ec10a5ef95eb7f151a1e86d41f55
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-23 08:06:36 +0700

    Create dog.js

---
+++ commands/dog.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+
+  let {body} = await superagent
+  .get(`https://random.dog/woof.json`);
+
+  let dogembed = new Discord.RichEmbed()
+  .setColor("#ffa202")
+  .setDescription("<a:Confused_Dog:511180901934301204> Here's the doggo! <a:cooldoge:511180988601073665>")
+  .setImage(body.url);
+
+  message.channel.send(dogembed);
+
+}
+
+module.exports.help = {
+  name: "dog"
+}
