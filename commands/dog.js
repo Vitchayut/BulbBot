5a31a4eec1405aefe7fd75c9099de25a976c777e
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-25 15:31:02 +0700

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -4,12 +4,12 @@ const superagent = require("superagent");
 module.exports.run = async (bot, message, args) => {
 
   let {body} = await superagent
-  .get(`https://random.dog/woof.json`);
+  .get(`https://dog.ceo/api/breeds/image/random`);
 
   let dogembed = new Discord.RichEmbed()
   .setColor("#ffa202")
   .setDescription("<a:Confused_Dog:511180901934301204> Here's the doggo! <a:cooldoge:511180988601073665>")
-  .setImage(body.url);
+  .setImage(body.message);
 
   message.channel.send(dogembed);
 
