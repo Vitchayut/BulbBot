723ff876e76a2a1b09060eb235bc8fe39daa54f7
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-25 17:29:59 +0700

    Create translate.js

---
+++ commands/translate.js
@@ -0,0 +1,64 @@
+const Discord = require('discord.js');
+let config = require("../botconfig.json");
+const translate = require('google-translate-api');
+const Langs = ['afrikaans','albanian','amharic','arabic','armenian','azerbaijani','bangla','basque','belarusian','bengali','bosnian','bulgarian','burmese','catalan','cebuano','chichewa','chinese simplified','chinese traditional','corsican','croatian','czech','danish','dutch','english','esperanto','estonian','filipino','finnish','french','frisian','galician','georgian','german','greek','gujarati','haitian creole','hausa','hawaiian','hebrew','hindi','hmong','hungarian','icelandic','igbo','indonesian','irish','italian','japanese','javanese','kannada','kazakh','khmer','korean','kurdish (kurmanji)','kyrgyz','lao','latin','latvian','lithuanian','luxembourgish','macedonian','malagasy','malay','malayalam','maltese','maori','marathi','mongolian','myanmar (burmese)','nepali','norwegian','nyanja','pashto','persian','polish','portugese','punjabi','romanian','russian','samoan','scottish gaelic','serbian','sesotho','shona','sindhi','sinhala','slovak','slovenian','somali','spanish','sundanese','swahili','swedish','tajik','tamil','telugu','thai','turkish','ukrainian','urdu','uzbek','vietnamese','welsh','xhosa','yiddish','yoruba','zulu'];
+
+module.exports.run = async (bot, message, args) => {
+
+  if (args[0] === undefined) {
+
+    const embed = new Discord.RichEmbed()
+    .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+    .setColor(config.red)
+    .setTimestamp()
+    .setDescription("**Provide a language and some text for bot to translate.**\nUsage: `--translate <language> <text>`");
+
+    return message.channel.send(embed);
+
+  } else {
+
+    if (args[1] === undefined) {
+
+      const embed2 = new Discord.RichEmbed()
+      .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+      .setColor(config.red)
+      .setTimestamp()
+      .setDescription("**Please give me something to translate.**\nUsage: `--translate <language> <text>`");
+
+      return message.channel.send(embed2);
+
+    } else {
+
+      let transArg = args[0].toLowerCase();
+
+      args = args.join(' ').slice(0);
+      let translation;
+
+      const embed3 = new Discord.RichEmbed()
+      .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+      .setColor(config.red)
+      .setTimestamp()
+      .setDescription("**Language not found.**");
+
+      if (!Langs.includes(transArg)) return message.channel.send(embed3);
+      args = args.slice(transArg.length);
+
+      translate(args, {to: transArg}).then(res => {
+
+        const embed = new Discord.RichEmbed()
+        .setDescription(res.text)
+        .setFooter(`Given -> ${transArg}`)
+        .setColor(`RANDOM`);
+        return message.channel.send(embed);
+
+      });
+
+    }
+
+  }
+
+}
+
+module.exports.help = {
+  name: "translate"
+}
