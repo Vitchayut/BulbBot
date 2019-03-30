0ed78ac80d3a21d07bf8fd3b6e73f4a6877aed6f
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-30 15:14:53 +0700

    Update hypixel.js

--- commands/hypixel.js
+++ commands/hypixel.js
@@ -1,14 +1,18 @@
 const Discord = require("discord.js");
-const HypixelAPI = require('hypixel-api')
-const client = new HypixelAPI(process.env.hypixel)
+const request = require("request");
+const moment = require('moment');
+const HypixelAPI = require('hypixel-api');
+const client = new HypixelAPI(process.env.hypixel);
 
 module.exports.run = async (bot, message, args) => {
+
+var json = JSON.parse(jsonString);
 
 let user = args.slice(0).join(" ");
 
 client.getPlayer('name', user).then((player) => {
     console.log(player)
-    message.reply(player)
+    message.reply(json.player.networkExp)
 }).catch((err) => {
     console.error('Error! ' + err)
     message.channel.send(`:no_entry: \`Usage: !player <name>\``)
