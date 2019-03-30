91ec47134b4e078d540196cb68ccaf0108cd89a7
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-30 19:29:03 +0700

    Update hypixel.js

--- commands/hypixel.js
+++ commands/hypixel.js
@@ -7,10 +7,10 @@ const client = new HypixelAPI(process.env.hypixel);
 module.exports.run = async (bot, message, args) => {
     
 let user = args.slice(0).join(" ");
+var json = JSON.parse(jsonString);
 
 client.getPlayer('name', user).then((player) => {
     console.log(player)
-    var json = JSON.parse(jsonString);
     message.reply(json.player.networkExp)
 }).catch((err) => {
     //console.error('Error! ' + err)
