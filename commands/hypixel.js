75a55ec4c19fcb1619a5b8c4a1eedf4670c5d4a5
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-30 12:35:55 +0700

    Update hypixel.js

--- commands/hypixel.js
+++ commands/hypixel.js
@@ -6,28 +6,17 @@ module.exports.run = async (bot, message, args) => {
 
 let user = args.slice(0).join(" ");
 
-try {  
-	hypixelPlayer = client.getPlayer(`name`, user).player
-}
-catch (err) {
-	console.log(err)
-	return
-}
-
+client.getPlayer('name', user).then((player) => {
+    console.log(player)
 let playerembed = new Discord.RichEmbed()
-  .setTitle(`Player: ` + hypixelPlayer.displayname)
+  .setTitle(`Player: ` + player.displayname)
   .setColor(`RANDOM`)
   .setTimestamp()
   message.channel.send(playerembed);
-//}
-//else {
-//	message.channel.send(`:no_entry: \`Usage: !player <name>\``)
-//}
-//client.getPlayer('name', user).then((player) => {
-//    console.log(player)
-//}).catch((err) => {
-//    console.error('Error! ' + err)
-//})    
+}).catch((err) => {
+    console.error('Error! ' + err)
+    message.channel.send(`:no_entry: \`Usage: !player <name>\``)
+})
 
 }
 
