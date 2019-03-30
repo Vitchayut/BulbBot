35a9988a7bfa3f1d524fc1756b61e742f533c642
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-30 14:15:45 +0700

    Update hypixel.js

--- commands/hypixel.js
+++ commands/hypixel.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 const HypixelAPI = require('hypixel-api')
-const client = new HypixelAPI('327aaf6b-0d25-47bb-bb70-ce3309c6aea0')
+const client = new HypixelAPI(hypixelapitoken)
 
 module.exports.run = async (bot, message, args) => {
 
 let user = args.slice(0).join(" ");
 
-client.getPlayer('name', user).then((player) => {
-    console.log(player)
+client.getPlayer('name', user).then((player.displayname) => {
+    console.log(player.displayname)
     message.reply(player.displayname)
 }).catch((err) => {
     console.error('Error! ' + err)
