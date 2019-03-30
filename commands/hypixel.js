c92fe9e772274629148c3d62f32efd401cb04431
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-03-30 12:12:56 +0700

    Update hypixel.js

--- commands/hypixel.js
+++ commands/hypixel.js
@@ -5,7 +5,7 @@ const client = new HypixelAPI('327aaf6b-0d25-47bb-bb70-ce3309c6aea0')
 module.exports.run = async (bot, message, args) => {
 
 let user = args.slice(0).join(" ");
-let hypixelPlayer = (await HypixelClient.getPlayer('name', user).player
+//let hypixelPlayer = (await HypixelClient.getPlayer('name', user).player
 
 let playerembed = new Discord.RichEmbed()
   .setTitle('Player: ' + hypixelPlayer.displayname)
