9a1da25b2a4516bd7b531e6a76a19d23c1b8b376
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-04-03 21:49:01 +0700

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const prefix = ("!");
+let config = require("../botconfig.json");
 const translate = require('google-translate-api');
 const Langs = ['afrikaans','albanian','amharic','arabic','armenian','azerbaijani','bangla','basque','belarusian','bengali','bosnian','bulgarian','burmese','catalan','cebuano','chichewa','chinese simplified','chinese traditional','corsican','croatian','czech','danish','dutch','english','esperanto','estonian','filipino','finnish','french','frisian','galician','georgian','german','greek','gujarati','haitian creole','hausa','hawaiian','hebrew','hindi','hmong','hungarian','icelandic','igbo','indonesian','irish','italian','japanese','javanese','kannada','kazakh','khmer','korean','kurdish (kurmanji)','kyrgyz','lao','latin','latvian','lithuanian','luxembourgish','macedonian','malagasy','malay','malayalam','maltese','maori','marathi','mongolian','myanmar (burmese)','nepali','norwegian','nyanja','pashto','persian','polish','portugese','punjabi','romanian','russian','samoan','scottish gaelic','serbian','sesotho','shona','sindhi','sinhala','slovak','slovenian','somali','spanish','sundanese','swahili','swedish','tajik','tamil','telugu','thai','turkish','ukrainian','urdu','uzbek','vietnamese','welsh','xhosa','yiddish','yoruba','zulu'];
 
@@ -8,8 +8,10 @@ module.exports.run = async (bot, message, args) => {
   if (args[0] === undefined) {
 
     const embed = new Discord.RichEmbed()
-    .setColor("FFFFFF")
-    .setDescription(":no_entry: \`Provide a language and some text for bot to translate.\`\n\`Usage: !translate <language> <text>\`");
+    .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+    .setColor(config.red)
+    .setTimestamp()
+    .setDescription("**Provide a language and some text for bot to translate.**\nUsage: `--translate <language> <text>`");
 
     return message.channel.send(embed);
 
@@ -17,16 +19,28 @@ module.exports.run = async (bot, message, args) => {
 
     if (args[1] === undefined) {
 
-      return message.channel.send(':no_entry: \`Please give me something to translate.\n!translate <language> <text>\`');
+      const embed2 = new Discord.RichEmbed()
+      .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+      .setColor(config.red)
+      .setTimestamp()
+      .setDescription("**Please give me something to translate.**\nUsage: `--translate <language> <text>`");
+
+      return message.channel.send(embed2);
 
     } else {
 
       let transArg = args[0].toLowerCase();
 
-      args = args.join(' ').slice(prefix.length);
+      args = args.join(' ').slice(0);
       let translation;
 
-      if (!Langs.includes(transArg)) return message.channel.send(`:no_entry: \`Language not found.\``);
+      const embed3 = new Discord.RichEmbed()
+      .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+      .setColor(config.red)
+      .setTimestamp()
+      .setDescription("**Language not found.**");
+
+      if (!Langs.includes(transArg)) return message.channel.send(embed3);
       args = args.slice(transArg.length);
 
       translate(args, {to: transArg}).then(res => {
