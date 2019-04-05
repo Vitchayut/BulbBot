7f510d2a1810dc5bdeddd0fa4d93b2f7939cc774
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-04-05 21:27:57 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
+let config = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
-    let version = `0.3.3a`;
     let bicon = bot.user.displayAvatarURL;
     let bname = bot.user.username;
     let botembed = new Discord.RichEmbed()
@@ -14,8 +14,8 @@ module.exports.run = async (bot, message, args) => {
     .setDescription("<:js:512247624490024986> Made with Node.js and Discord.js <:discordjs:553157782652715009>\n<:Heroku:553158576458432523> Hosted by Heroku.")
     .setFooter(`Made by _ItsNuaZ#3977`, `https://cdn.discordapp.com/avatars/346102251632197632/b92c95190ff53e40c396104534d71f75.png`)
     .addField(`:clipboard: Created by`, `_ItsNuaZ#3977`, true)
-    .addField(`:information_source: Version`, `${version}`, true)
-    .addField(`:tools: Debug`, `New update to version ${version}!, alot of stuff have been changed and improved. :D`)
+    .addField(`:information_source: Version`, `${config.version}`, true)
+    .addField(`:tools: Debug`, `New update to version ${config.version}!, alot of stuff have been changed and improved. :D`)
     .setTimestamp();
 
     message.channel.send(botembed);
