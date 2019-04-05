e8612883fe31010520c80e761afed6a7a8fdd708
NuaZaKung <44229262+NuaZaKung@users.noreply.github.com>
2019-04-05 16:06:40 +0700

    Update roblox.js

--- commands/roblox.js
+++ commands/roblox.js
@@ -45,9 +45,11 @@ module.exports.run = async (bot, message, args) => {
                     }
 
                     var friends = names.join(", ");**/
-                      if (body2.IsOnline === false) body2.IsOnline = '<:discord_invisible:553168206249066496> \`Offline\`';
-                      if (body2.IsOnline === true) body2.IsOnline = '<:discord_online:553168186925907980> \`Online\`';
-                      
+                    
+                      var status = body2.IsOnline
+                      if (status === null) status = ':grey_question: \`Unknown\`';
+                      if (status === true) status = '<:discord_online:553168186925907980> \`Online\`';
+                      if (status === false) status = '<:discord_invisible:553168206249066496> \`Offline\`';
                       const embed = new Discord.RichEmbed()
                       .setTitle(`<:roblox:563611416473501716> **` + body2.Username + `'s profile` + `**`)
                       .setURL("https://www.roblox.com/users/" + body2.Id + "/profile")
@@ -64,7 +66,7 @@ module.exports.run = async (bot, message, args) => {
                        */
                       .setTimestamp()
                       .addField(":name_badge: Username", `\`` + body2.Username + `\``, true)                    
-                      .addField(":video_game: Status", body2.IsOnline, true)
+                      .addField(":video_game: Status", status, true)
                       .addField(":card_index: ID", `\`` + body2.Id + `\``, true)
                       /*
                        * Inline fields may not display as inline if the thumbnail and/or image is too big.
