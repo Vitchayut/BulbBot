84f3d7800c6df44ae79214f2b8d4f47a165d9413
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-04-15 09:14:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ bot.on("message", async message => {
   let prefix = prefixes[message.guild.id].prefixes;
   
   // Blacklisted words.
-  let blacklisted = ["fuck", "faggot", "nigga", "nigger", "pussy", "rape", "dick", "pussi", "porn", "dildo", "nazi", "hitler", "penis", "boob", "cunt", "cum", "bitch", "nude", "cock", "twat", "hentai", "anal", "spank", "blowjob", "futanari", "vagina"];
+  /**let blacklisted = ["fuck", "faggot", "nigga", "nigger", "pussy", "rape", "dick", "pussi", "porn", "dildo", "nazi", "hitler", "penis", "boob", "cunt", "cum", "bitch", "nude", "cock", "twat", "hentai", "anal", "spank", "blowjob", "futanari", "vagina"];
   let foundInText = false;
   for (var i in blacklisted) {
       if (message.content.toLowerCase().includes(blacklisted[i].toLowerCase())) foundInText = true;
@@ -105,7 +105,7 @@ bot.on("message", async message => {
       .setColor("#f44242")
       .addField("<:Content_Blocked:523798974876876810> \`Your message contains off-site links, deleted.\` <a:BoiGifFixed:511160003667689484>", message.author)
       message.channel.send(offsitestuff).then(msg => {msg.delete(10850)});
-  }
+  }**/
 
   if(!message.content.startsWith(prefix)) return;
   // Cooldown feature.
