c9bda39e6322a26c8a9601face78f011f68dca63
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-04-15 13:19:34 +0700

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -10,10 +10,10 @@ module.exports.run = async (bot, message, args) => {
 message.delete();
 if(!message.member.hasPermission("BAN_MEMBERS")) return errors.noPerms(message, "Ban Members");
 let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-if(!bUser) return errors.cantfindUser(message.channel);
-if(bUser.id === bot.user.id) return errors.botuser(message.channel);
+if(!bUser) return errors.cantfindUser(message);
+if(bUser.id === bot.user.id) return errors.botuser(message);
 let bReason = args.join(" ").slice(22);
-if(!bReason) return errors.noReason(message.channel);
+if(!bReason) return errors.noReason(message);
 if(bUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, bUser, "Manage Messages");
 if(!args[0] || args[0] == "help"){
 message.reply(`:no_entry: \`Usage: --ban <user> <reason>\``);
