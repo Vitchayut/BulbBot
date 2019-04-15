f1da0d6d9b2d474e28214159d499e390c9d71e57
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-04-15 12:08:58 +0700

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+const botconfig = require("../botconfig.json");
+const fs = require("fs");
+const red = botconfig.red;
+const green = botconfig.green;
+const orange = botconfig.orange;
+
+module.exports.run = async (bot, message, args) => {
+message.delete();
+if(!message.member.hasPermission("BAN_MEMBERS")) return errors.noPerms(message, "Ban Members");
+let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!bUser) return errors.cantfindUser(message.channel);
+if(bUser.id === bot.user.id) return errors.botuser(message.channel);
+let bReason = args.join(" ").slice(22);
+if(!bReason) return errors.noReason(message.channel);
+if(bUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, bUser, "Manage Messages");
+if(!args[0] || args[0] == "help"){
+message.reply(`:no_entry: \`Usage: --ban <user> <reason>\``);
+  return;
+}
+
+let banEmbed = new Discord.RichEmbed()
+.setAuthor(`Ban | ${bUser.user.tag}`, `${bUser.user.displayAvatarURL}`)
+.setColor(red)
+.setTimestamp()
+.setDescription(`**Banned user:** ${bUser} (${bUser.id})\n**Banned by:** <@${message.author.id}> (${message.author.id})\n**Reason:** ${bReason}`)
+
+let channelnotdetect = new Discord.RichEmbed()
+.setAuthor(message.member.displayName, message.author.displayAvatarURL)
+.setColor(red)
+.setTimestamp()
+.setDescription(`<:red_tick:566946004948090880> Can't find logs channel, set a logs channel first!\n<a:righter_arrow:518744759506960406> Usage: --setlog (channel) <a:lefter_arrow:518744793489342464>`);
+let rcs = JSON.parse(fs.readFileSync("./rcs.json", "utf8"));
+let reportschannel = "";
+if(rcs[message.guild.id]) reportschannel = message.guild.channels.find(c => c.name === rcs[message.guild.id].rc);
+if(!reportschannel) return message.channel.send(channelnotdetect);
+
+message.delete().catch(O_o=>{});
+message.guild.member(bUser).ban(bReason);
+reportschannel.send(banEmbed);
+
+}
+
+module.exports.help = {
+  name: "ban"
+}
