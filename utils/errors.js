45c6abe87d1c935654357bd38b1af8e187890bb5
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-04-15 12:18:07 +0700

    Update errors.js

--- utils/errors.js
+++ utils/errors.js
@@ -6,7 +6,7 @@ module.exports.noPerms = (message, perm) => {
     let embed = new Discord.RichEmbed()
         .setAuthor(message.member.displayName, message.author.displayAvatarURL)
         .setColor(config.red)
-        .setDescription(`<:poll_disagree:437529095065174020> \`Insufficient Permissions!\`\n<a:Question:521253226754867224> \`Required: ${perm}\``, true);
+        .setDescription(`<:red_tick:566946004948090880> \`Insufficient Permissions!\`\n<a:Question:521253226754867224> \`Required: ${perm}\``, true);
 
     message.channel.send(embed).then(m => m.delete(8000));
 }
@@ -16,7 +16,7 @@ module.exports.equalPerms = (message, user, perms) => {
     let embed = new Discord.RichEmbed()
         .setAuthor(message.member.displayName, message.author.displayAvatarURL)
         .setColor(config.red)
-        .setDescription(`<:poll_disagree:437529095065174020> ${user} \`have moderator permissions!\``, `<a:Question:521253226754867224> \`Permission: ${perms}\``);
+        .setDescription(`<:red_tick:566946004948090880> ${user} \`have moderator permissions!\``, `<a:Question:521253226754867224> \`Permission: ${perms}\``);
 
     message.channel.send(embed).then(m => m.delete(8000));
 
@@ -24,45 +24,45 @@ module.exports.equalPerms = (message, user, perms) => {
 
 module.exports.botuser = (message) => {
     let embed = new Discord.RichEmbed()
-        .setAuthor('Error!', 'https://freeiconshop.com/wp-content/uploads/edd/error-flat.png')
-        .setDescription("<:poll_disagree:437529095065174020> \`You cannot ban a bot!\`")
+        .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+        .setDescription("<:red_tick:566946004948090880> \`You cannot ban a bot!\`")
         .setColor(config.red);
 
     message.channel.send(embed).then(m => m.delete(8000));
 }
 
-module.exports.cantfindUser = (channel) => {
+module.exports.cantfindUser = (message) => {
     let embed = new Discord.RichEmbed()
-        .setAuthor('Error!', 'https://freeiconshop.com/wp-content/uploads/edd/error-flat.png')
-        .setDescription("<:poll_disagree:437529095065174020> \`Could not find that user.\`")
+        .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+        .setDescription("<:red_tick:566946004948090880> \`Could not find that user.\`")
         .setColor(config.red);
 
-    channel.send(embed).then(m => m.delete(8000));
+    message.channel.send(embed).then(m => m.delete(8000));
 }
 
-module.exports.cantfindUserID = (channel) => {
+module.exports.cantfindUserID = (message) => {
     let embed = new Discord.RichEmbed()
-        .setAuthor('Error!', 'https://freeiconshop.com/wp-content/uploads/edd/error-flat.png')
-        .setDescription("<:poll_disagree:437529095065174020> \`Could not find that user.\`\n<a:Question:521253226754867224> \`Please enter a User ID to unban.\`")
+        .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+        .setDescription("<:red_tick:566946004948090880> \`Could not find that user.\`\n<a:Question:521253226754867224> \`Please enter a User ID to unban.\`")
         .setColor(config.red);
 
-    channel.send(embed).then(m => m.delete(8000));
+    message.channel.send(embed).then(m => m.delete(8000));
 }
 
-module.exports.noReason = (channel) => {
+module.exports.noReason = (message) => {
     let embed = new Discord.RichEmbed()
-        .setAuthor('Error!', 'https://freeiconshop.com/wp-content/uploads/edd/error-flat.png')
-        .setDescription("<:poll_disagree:437529095065174020> \`Please supply a reason.\`")
+        .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+        .setDescription("<:red_tick:566946004948090880> \`Please supply a reason.\`")
         .setColor(config.red);
 
-    channel.send(embed).then(m => m.delete(8000));
+    message.channel.send(embed).then(m => m.delete(8000));
 }
 
 module.exports.onlyOwner = (message) => {
     let embed = new Discord.RichEmbed()
         .setColor(config.red)
         .setAuthor(message.member.displayName, message.author.displayAvatarURL)
-        .setDescription("<:poll_disagree:437529095065174020> Only <@346102251632197632> can execute this command!");
+        .setDescription("<:red_tick:566946004948090880> Only <@346102251632197632> can execute this command!");
 
     message.channel.send(embed).then(m => m.delete(8000));
 }
@@ -71,7 +71,7 @@ module.exports.onlyDonator = (message) => {
     let embed = new Discord.RichEmbed()
         .setColor(config.red)
         .setAuthor(message.member.displayName, message.author.displayAvatarURL)
-        .setDescription("<:poll_disagree:437529095065174020> Only donators can execute this command!");
+        .setDescription("<:red_tick:566946004948090880> Only donators can execute this command!");
 
     message.channel.send(embed).then(m => m.delete(8000));
 }
