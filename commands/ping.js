f5e0607fedbfe536981662f0b8cfd0c00486091c
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-04-15 16:47:59 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,14 @@
 const Discord = require("discord.js");
+let config = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
 
-message.channel.send(`:ping_pong: Pong! \`${bot.pings[0]}ms\``);
+let embed = new Discord.RichEmbed()
+.setAuthor(message.member.displayName, message.author.displayAvatarURL)
+.setColor(config.gold)
+.setTimestamp()
+.setDescription(`:ping_pong: Pong! \`${bot.pings[0]}ms\``);
+message.channel.send(embed)
 
 }
 module.exports.help = {
