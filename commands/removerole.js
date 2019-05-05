32367494cf927660ef32662016a7506dc128adf6
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-05-05 11:40:06 +0700

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -10,20 +10,20 @@ module.exports.run = async (bot, message, args) => {
     return;
   }
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if (!rMember) return errors.cantfindUser(message.channel);
+  if (!rMember) return errors.cantfindUser(message);
   let role = args.join(" ").slice(22);
   if (!role) return message.reply(`:no_entry: \`Specify a role!\``);
   let gRole = message.mentions.roles.first() || message.guild.roles.find(`name`, args[1]);
   if (!gRole) return message.reply(`:no_entry: \`Couldn't find that role.\``);
 
   if (rMember.roles.has(gRole.id)) return message.reply(`:no_entry: \`They already have that role.\``);
-  await (rMember.addRole(gRole.id));
+  await (rMember.removeRole(gRole.id));
 
   try {
-    await rMember.send(`<:green_tick:566945998761361408> \`${gRole.name}\``)
+    await rMember.send(`<:green_tick:566945998761361408> \`You lost the role, ${gRole.name}\``)
   } catch (e) {
     console.log(e.stack);
-    message.channel.send(`<:green_tick:566945998761361408> \`R.I.P to <@${rMember.id}>, their role ${gRole.name} have been taken away!\nWe tried to DM them, but their DMs are locked.\``)
+    message.channel.send(`<:green_tick:566945998761361408> \`**R.I.P** to <@${rMember.id}>, the role ${gRole.name} have been taken away!\nWe tried to DM them, but their DMs are locked.\``)
   }
 }
 
