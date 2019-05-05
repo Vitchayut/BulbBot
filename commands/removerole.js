e7170c276ee0fade7e373ea6d66edd29a481e252
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-05-05 12:02:09 +0700

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
+let config = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
 
-  //!addrole @andrew Dog Person
+  //!removerole @andrew @Mod
   if (!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "Administrator");
   if (args[0] == "help") {
-    message.reply(`:no_entry: \`Usage: !addrole <user> <role>\``);
+    message.reply(`:no_entry: \`Usage: !removerole <user> <role>\``);
     return;
   }
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
@@ -20,10 +21,20 @@ module.exports.run = async (bot, message, args) => {
   await (rMember.removeRole(gRole.id));
 
   try {
-    await rMember.send(`<:green_tick:566945998761361408> \`You lost the role, ${gRole.name}\``)
+    let respondEmbedDM = new Discord.RichEmbed()
+    .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+    .setColor(config.red)
+    .setTimestamp()
+    .setDescription(`<:red_tick:566946004948090880> You lost the role, \`${gRole.name}\``);
+    await rMember.send(respondEmbedDM)
   } catch (e) {
     console.log(e.stack);
-    message.channel.send(`<:green_tick:566945998761361408> \`**R.I.P** to <@${rMember.id}>, the role ${gRole.name} have been taken away!\nWe tried to DM them, but their DMs are locked.\``)
+    let respondEmbed = new Discord.RichEmbed()
+    .setAuthor(message.member.displayName, message.author.displayAvatarURL)
+    .setColor(config.green)
+    .setTimestamp()
+    .setDescription(`<:green_tick:566945998761361408> Successfully removed the role, ${gRole} from \`${rMember.tag}\``);
+    message.channel.send(respondEmbed)
   }
 }
 
