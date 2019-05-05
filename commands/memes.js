026771f548a9aebb92c586872eca760c0817d4c3
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-05-05 18:19:25 +0700

    Update memes.js

--- commands/memes.js
+++ commands/memes.js
@@ -1,15 +1,16 @@
 const Discord = require("discord.js");
-const superagent = require("superagent");
+//const superagent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
 
-  let {body} = await superagent
-  .get(`https://api-to.get-a.life/meme`);
-
+  //let {body} = await superagent
+  //.get(`https://api-to.get-a.life/meme`);
+  let cat = `https://referralrock.com/blog/wp-content/uploads/2017/07/cat.jpg`;
+  
   let memesembed = new Discord.RichEmbed()
   .setAuthor(message.member.displayName, message.author.displayAvatarURL)
   .setColor('RANDOM')
-  .setImage(body.url);
+  .setImage(cat);
 
   message.channel.send(memesembed);
 
