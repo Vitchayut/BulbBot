7ddbb643d9755855d6207d0056b6a9dd3063f96a
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-05-05 19:45:53 +0700

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
   if (!rMember) return errors.cantfindUser(message);
   let role = args.join(" ").slice(22);
   if (!role) return message.reply(`:no_entry: \`Specify a role!\``);
-  let gRole = message.mentions.roles.first() || message.guild.roles.find(`name`, args[1]);
+  let gRole = message.mentions.roles.first() || message.guild.roles.find(role => role.name === args[1]);
   if (!gRole) return message.reply(`:no_entry: \`Couldn't find that role.\``);
 
   if (rMember.roles.has(gRole.id)) return message.reply(`:no_entry: \`They already have that role.\``);
