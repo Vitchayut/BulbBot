1f76d1c32ecdb80ab5eee733dc9d2b6fda7b2451
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-05-05 18:39:27 +0700

    Create news.js

---
+++ commands/news.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+let config = require("../botconfig.json");
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.newsapi);
+
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+newsapi.v2.topHeadlines({
+  sources: 'bbc-news,the-verge',
+  q: 'bitcoin',
+  category: 'business',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});
+
+}
+
+module.exports.help = {
+  name: "news"
+}
