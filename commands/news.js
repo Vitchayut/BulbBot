b8d2acc8b86952723e35c506763f4d5cc5fbd9c0
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-05-06 12:52:47 +0700

    Update news.js

--- commands/news.js
+++ commands/news.js
@@ -1,20 +1,17 @@
 const Discord = require("discord.js");
 let config = require("../botconfig.json");
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI(process.env.newsapi);
 
 module.exports.run = async (bot, message, args) => {
 
-newsapi.v2.topHeadlines({
-  q: 'trump',
-  category: 'politics',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-});
+let newsembed = new Discord.RichEmbed()
+.setAuthor(message.member.displayName, message.author.displayAvatarURL)
+.setColor(config.blue)
+.addField(`Title`, `Description`, true)
+.setTimestamp()
+message.channel.send(newsembed);
 
 }
+
 module.exports.help = {
   name: "news"
 }
