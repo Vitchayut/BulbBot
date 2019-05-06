b7faea15374b4f989d93f274e4b612e8f0ff9db3
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-05-06 12:59:47 +0700

    Update news.js

--- commands/news.js
+++ commands/news.js
@@ -7,6 +7,7 @@ let newsembed = new Discord.RichEmbed()
 .setAuthor(message.member.displayName, message.author.displayAvatarURL)
 .setColor(config.blue)
 .addField(`Title`, `Description`, true)
+.setThumbnail(config.ownerURL)
 .setTimestamp()
 message.channel.send(newsembed);
 
