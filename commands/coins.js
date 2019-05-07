b83c067e33cb2e356384bb48ee1672bc10f3d4d8
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-05-07 15:45:43 +0700

    Update coins.js

--- commands/coins.js
+++ commands/coins.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   //!coins
   let user = message.mentions.users.first() || message.author;
 
-  if(user.id === bot.user.id) return message.reply(`:no_entry: \`You cannot do that with bots!\``);
+  if(user.id === message.author.bot) return message.reply(`:no_entry: \`You cannot do that with bots!\``);
   
   if(!coins[user.id]){
     coins[user.id] = {
