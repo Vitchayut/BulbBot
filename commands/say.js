ccb9709661710274a6c8a9f4d1c711c9e1e88822
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-05-08 11:44:09 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  message.delete();
+  await message.delete();
   if(!message.member.hasPermission(`MANAGE_MESSAGES`)) return message.reply(`:no_entry: \`Insufficient Permissions!\``);
   let botmessage = args.join(" ");
   message.channel.send(botmessage);
