be096b7ce85bfb28c9d717b5d0cf7fa4190f9431
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-08-06 21:10:08 +0700

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -1,12 +1,8 @@
 const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
-    // DD/MM/YY format
-    let day = message.guild.createdAt.getDate()
-    let month = 1 + message.guild.createdAt.getMonth()
-    let year = message.guild.createdAt.getFullYear()
     // Lets define our array of guilds
     const guildArray = bot.guilds.map((guild) => {
-    return `Guild Name • ${guild.name} : Guild ID • ${guild.id} : Owner • ${guild.owner.user.tag} : Server Created • ${day}.${month}.${year}`
+    return `Guild Name • ${guild.name} : Guild ID • ${guild.id} : Owner • ${guild.owner.user.tag} : Server Created • ${guild.createdAt.getDate()}.${guild.createdAt.getMonth() + 1}.${guild.createdAt.getFullYear()}`
     })
 
     // And send it
