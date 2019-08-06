84e11f5405696b8e3307d8cd30661c24275103ce
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-08-06 19:41:28 +0700

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -42,7 +42,7 @@ message.channel.send(respondEmbed);
 message.delete().catch(O_o=>{});
   
 const logChannel = message.guild.channels.find(ch => ch.name === '📋staff-log📋');
-if (!logChannel) return message.reply(reportEmbed);
+if (!logChannel) return message.channel.send(reportEmbed);
 logChannel.send(reportEmbed);
   
 /**Log.findOne({
