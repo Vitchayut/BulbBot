73ffc6791d52e219402ce1c5b44ac675eec214ed
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-08-07 18:14:15 +0700

    Update forceleave.js

--- commands/forceleave.js
+++ commands/forceleave.js
@@ -3,9 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 
   if (args.length  < 1) return message.reply("You must supply a Guild ID");
-  if (message.author.id !== "346102251632197632") return message.reply(":no_entry: `No permission to use this command!`");
+  if (message.author.id !== "346102251632197632") return message.reply(`:no_entry: \`No permission to use this command!\``);
   client.guilds.get(args.join(" ")).leave()
   .then(g => console.log(`Left the guild ${g}`)).catch(console.error);
+  message.reply(`<:green_tick:566945998761361408> \`Successfully left that guild!\``);
   
 }
 
