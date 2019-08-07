4cd9f7894314ac744489fc2decb84928d087229c
ItsNuaZ <44229262+ItsNuaZ@users.noreply.github.com>
2019-08-07 18:13:07 +0700

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -1,12 +1,14 @@
 const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
+    if (message.author.id !== "346102251632197632") return message.reply(`:no_entry: \`No permission to use this command!\``);
     // Lets define our array of guilds
     const guildArray = bot.guilds.map((guild) => {
     return `Name • ${guild.name}\nID • ${guild.id}\nOwner • ${guild.owner.user.tag}\nCreated • ${guild.createdAt.getDate()}.${guild.createdAt.getMonth() + 1}.${guild.createdAt.getFullYear()}\n====================================================================================================`
     })
 
     // And send it
-    console.log(`${guildArray.join("\n")}`)
+    console.log(`${guildArray.join("\n")}`);
+    message.reply(`<:green_tick:566945998761361408> \`Check the console log for info!\``);
 
 }
 
