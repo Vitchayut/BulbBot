ab8565243a9fcc30f62d625d12a6fc08fd1f8efe
Vitchayut2002 <48343358+Vitchayut2002@users.noreply.github.com>
2019-08-16 19:36:18 +0700

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
-    if (message.author.id !== "346102251632197632") return message.reply(`:no_entry: \`No permission to use this command!\``);
+    if (message.author.id !== "263233275479195648") return message.reply(`:no_entry: \`No permission to use this command!\``);
     // Lets define our array of guilds
     const guildArray = bot.guilds.map((guild) => {
     return `Name • ${guild.name}\nID • ${guild.id}\nOwner • ${guild.owner.user.tag}\nCreated • ${guild.createdAt.getDate()}.${guild.createdAt.getMonth() + 1}.${guild.createdAt.getFullYear()}\n====================================================================================================`
