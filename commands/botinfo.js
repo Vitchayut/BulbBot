5257b085bc37368fed33b45ec68cb4b2a106e064
Vitchayut2002 <48343358+Vitchayut2002@users.noreply.github.com>
2019-08-17 18:35:33 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
     .setTitle(`Click here to join our discord server!`)
     .setDescription("<:js:512247624490024986> Made with Node.js and Discord.js <:discordjs:553157782652715009>\n<:Heroku:553158576458432523> Hosted by Heroku.")
     .setFooter(`Made by _ItsNuaZ#3977 & Vitchayut#8695`, `https://cdn.discordapp.com/avatars/346102251632197632/b92c95190ff53e40c396104534d71f75.png`)
-    .addField(`:clipboard: Created by`, `_ItsNuaZ#3977 & vITCHAYUT#8695`, true)
+    .addField(`:clipboard: Created by`, `_ItsNuaZ#3977 & Vitchayut#8695`, true)
     .addField(`:information_source: Version`, `${config.version}`, true)
     .addField(`:tools: Debug`, `New update to version ${config.version}!, alot of stuff have been changed and improved. :D`)
     .setTimestamp();
